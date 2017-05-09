986c54a61fe6414c35082260025ce8eb06a8f457
JasonWang0606 <465545121@qq.com>
2017-05-09 22:52:57 +0800

    init

--- fullscreenswitch.js
+++ fullscreenswitch.js
@@ -11,8 +11,71 @@
 		}
 
 		pageSwitch.prototype = {
+			// 初始化插件
+			// 实现：初始化dom结构、布局、分页及绑定事件
 			init : function () {
-				console.log("hello");
+				var me = this;
+				me.selectors = me.settings.selectors;
+				me.sections = me.selectors.sections;
+				me.section = me.selectors.section;
+
+				me.direction = me.selectors.direction === "vertical" ? true : false;
+				me.pagesCount = me.pagesCount();
+				me.index = (me.settings.index>=0 && me.settings.index < pagesCount) ? me.settings.index : 0;
+				if (!me.direction) {
+					me._initLayout();
+				}
+
+				if (me.settings.pagination) {
+					me._initPaging();
+				}
+
+				me.initEvent();
+			},
+
+			// 获取滑动页面数量*/
+			pagesCount : function () {
+				return this.section.length;
+			},
+
+			// 获取滑动的宽度（横屏滑动）或高度（竖屏滑动）
+			switchLength : function () {
+				return this.direction ? this.elem.height() : this.elem.width();
+			},
+
+			// 主要针对横屏情况进行页面布局
+			_initLayout : function () {
+				var me = this,
+					width = (me.pagesCount * 100 + "%"),
+					cellWidth = (100/me.pagesCount).toFixed(2) + "%";
+				me.sections.width(width);
+				me.section.width(cellWidth).css("float","left");
+			},
+
+			// 实现分页的dom结构及css样式 
+			_initPaging : function () {
+				var me = this,
+					pagesClass = me.settings.page.substring(1),
+					activeClass = me.settings.active.substring(1);
+				var pageHtml = "<ul class="+pagesClass+">";
+				for (var i=0;i<me.pagesCount;i++) {
+					pageHtml += "<li></li>"
+				}
+				me.element.append(pageHtml);
+				var pages = me.element.find(me.selectors.page);
+				me.pageItem = pages.find("li");
+				me.pageItem.eq(me.index).addClass(me.activeClass);
+
+				if (me.direction) {
+					pages.addClass("vertical");
+				} else {
+					pages.addClass("herizontal");
+				}
+			},
+
+			// 初始化插件事务
+			_initEvent : function () {
+
 			}
 		}
 		return pageSwitch;
@@ -58,8 +121,8 @@
 		pagination : true,
 		// 是否触发键盘事件
 		keyboard : true,
-		// 滑动方向，默认垂直
-		direction : "vertical",
+		// 滑动方向，默认垂直vertical 为true，herizontal 为false
+		direction : true,
 		callback :  ""
 	}
 })(jQuery)
